e2bf027da7e94368ddd25e88552b648d260b55c4
Laszlo Csele <laszlo.csele90@gmail.com>
2019-02-15 20:06:14 +0100

    feat(init): init vue dotenv poc app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>{{ config }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { convict } from './config/convict';
+
+@Component({})
+export default class App extends Vue {
+  config = convict;
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
